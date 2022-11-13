2ccc8be9f342f9bf39f9c2232f523ea0e0b6d8bf
SOLA <codkan20@gmail.com>
2022-11-13 19:28:55 +0900

    계속 하는즁

--- do_it_prj/blog/models.py
+++ do_it_prj/blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=30) #제목은 문자를 담는 필드로, 최대 30글자까지
     content = models.TextField() #내용은 문자열의 길이 제한이 없는 텍스트 필드
+    head_image=models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True) #작성일은 월일시분초까지 기록할 수 있게 해주는 타임 필드
     update_at = models.DateTimeField(auto_now=True)
     # author : 추후 작성 예정
--- do_it_prj/blog/urls.py
+++ do_it_prj/blog/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
     # path('<int:pk>/', views.single_post_page),
-    
+
 ]
