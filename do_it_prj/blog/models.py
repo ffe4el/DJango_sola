b9572711b968c5a023ae87d655346ff83a0aa2e2
SOLA <codkan20@gmail.com>
2022-11-13 19:57:54 +0900

    Post모델에 파일업로드 필드 추가하기

--- do_it_prj/blog/models.py
+++ do_it_prj/blog/models.py
@@ -4,6 +4,7 @@ class Post(models.Model):
     title = models.CharField(max_length=30) #제목은 문자를 담는 필드로, 최대 30글자까지
     content = models.TextField() #내용은 문자열의 길이 제한이 없는 텍스트 필드
     head_image=models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True) #작성일은 월일시분초까지 기록할 수 있게 해주는 타임 필드
     update_at = models.DateTimeField(auto_now=True)
     # author : 추후 작성 예정
