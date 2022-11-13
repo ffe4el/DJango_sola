129ede43f544be88043b9bb0cf160e5bce008bd4
SOLA <codkan20@gmail.com>
2022-11-13 17:28:36 +0900

    /blog에 글 보이게...

---
+++ do_it_prj/blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # 이부분을 채워서 urls 주소를 불러 줄것이다~!
+    #FBV로 페이지 만들기
+    path('', views.index),
+    # path('', views.PostList.as_view()),
+    # path('<int:pk>/', views.single_post_pages),
+]
--- do_it_prj/blog/views.py
+++ do_it_prj/blog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
+#FBV로 페이지 만들기
+def index(request):
+    posts = Post.objects.all().order_by('-pk') #모든 Post 레코드를 가져와 posts에 저장
+    #ㄴ 이런 방식으로 views.py에서 데이터베이스에 쿼리를 날려 원하는 레코드를 가져올 수 있다.
+
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts' : posts, #render()함수안에 posts를 딕셔너리 형태로 추가
+        }
+    )
+
+#CBV로 페이지 만들기
+# class PostList(ListView):
+#     model = Post #ListView를 사용할 것이고, model은 Post다.
+#     template_name = 'blog/index.html'
 # Create your views here.
