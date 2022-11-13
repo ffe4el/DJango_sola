bcab7094521fa400d29c7a76351ed2b2b2fed79f
SOLA <codkan20@gmail.com>
2022-11-13 18:42:02 +0900

    CBV로 블로그 포스트 상세 페이지 만들기

--- do_it_prj/blog/urls.py
+++ do_it_prj/blog/urls.py
@@ -5,10 +5,12 @@ urlpatterns = [
     # 이부분을 채워서 urls 주소를 불러 줄것이다~!
 
     #FBV로 페이지 만들기
-    path('<int:pk>/', views.single_post_page),
-    path('', views.index),
+    # path('<int:pk>/', views.single_post_page),
+    # path('', views.index),
 
     #CBV로 페이지 만들기
-    # path('', views.PostList.as_view()),
-    # path('<int:pk>/', views.single_post_pages),
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('', views.PostList.as_view()),
+    # path('<int:pk>/', views.single_post_page),
+    
 ]
--- do_it_prj/blog/views.py
+++ do_it_prj/blog/views.py
@@ -1,32 +1,36 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 #FBV로 페이지 만들기
-def index(request):
-    posts = Post.objects.all().order_by('-pk') #모든 Post 레코드를 가져와 posts에 저장
-    #ㄴ 이런 방식으로 views.py에서 데이터베이스에 쿼리를 날려 원하는 레코드를 가져올 수 있다.
-
-    return render(
-        request,
-        'blog/landing.html',
-        {
-            'posts' : posts, #render()함수안에 posts를 딕셔너리 형태로 추가
-        }
-    )
-
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post': post,
-        }
-    )
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk') #모든 Post 레코드를 가져와 posts에 저장
+#     #ㄴ 이런 방식으로 views.py에서 데이터베이스에 쿼리를 날려 원하는 레코드를 가져올 수 있다.
+#
+#     return render(
+#         request,
+#         'blog/landing.html',
+#         {
+#             'posts' : posts, #render()함수안에 posts를 딕셔너리 형태로 추가
+#         }
+#     )
+#
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+#     return render(
+#         request,
+#         'blog/post_detail.html',
+#         {
+#             'post': post,
+#         }
+#     )
 
 #CBV로 페이지 만들기
-# class PostList(ListView):
-#     model = Post #ListView를 사용할 것이고, model은 Post다.
-#     template_name = 'blog/landing.html'
+class PostList(ListView):
+    model = Post #ListView를 사용할 것이고, model은 Post다.
+    ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
+
 # Create your views here.
