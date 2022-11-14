133a69076a854791f6235d09dda4b108b864bb44
SOLA <codkan20@gmail.com>
2022-11-14 21:55:57 +0900

    다대일

--- do_it_prj/blog/models.py
+++ do_it_prj/blog/models.py
@@ -2,6 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 import os
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
 class Post(models.Model):
     title = models.CharField(max_length=30) #제목은 문자를 담는 필드로, 최대 30글자까지
     hook_text = models.CharField(max_length=100, blank=True)
@@ -13,6 +23,7 @@ class Post(models.Model):
 
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL) #CASCADE는 작성자가 데이터베이스에서 삭제되었을때 이 포스트도 같이 삭제한다라는 의미.
                                                                 #SET_NULL은 작성자가 데이터베이스에서 삭제되었을때 이 포스트의 작성자명을 null값으로 둔다는 의미.
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
 
 
     def __str__(self):
