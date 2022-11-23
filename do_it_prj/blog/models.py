97f0cff0d9845c8f3a111c6125c39ef8aff33050
SOLA <93892724+ffe4el@users.noreply.github.com>
2022-11-23 18:47:48 +0900

    Update models.py

--- do_it_prj/blog/models.py
+++ do_it_prj/blog/models.py
@@ -15,6 +15,16 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'Categories'
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/tag/{self.slug}/'
+
 class Post(models.Model):
     title = models.CharField(max_length=30) #제목은 문자를 담는 필드로, 최대 30글자까지
     hook_text = models.CharField(max_length=100, blank=True)
@@ -27,6 +37,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL) #CASCADE는 작성자가 데이터베이스에서 삭제되었을때 이 포스트도 같이 삭제한다라는 의미.
                                                                 #SET_NULL은 작성자가 데이터베이스에서 삭제되었을때 이 포스트의 작성자명을 null값으로 둔다는 의미.
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    tags = models.ManyToManyField(Tag, blank=True)
 
 
     def __str__(self):
@@ -42,3 +53,4 @@ class Post(models.Model):
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1] #확장자 찾기
 # Create your models here.
+
