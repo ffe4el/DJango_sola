13f9738833a71c47799e65993a6eba741011f9db
SOLA <93892724+ffe4el@users.noreply.github.com>
2022-11-25 18:32:53 +0900

    포스트를 작성할 때 텍스트로 태그 지정하기

--- do_it_prj/blog/urls.py
+++ do_it_prj/blog/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     # path('', views.index),
 
     #CBV로 페이지 만들기
+    path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('create_post/', views.PostCreate.as_view()),
     path('tag/<str:slug>/', views.tag_page),
     path('category/<str:slug>/', views.category_page),
